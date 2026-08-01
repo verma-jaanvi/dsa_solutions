bfe494219d6c1b3ea3957e4af65923841cbb188f
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-08-01 12:38:08 +0530

    Time: 1 ms (11.84%) | Memory: 15.1 MB (22.31%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL)    return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> ans;
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            if(curr != NULL){
+                if(!q.empty() && q.front() == NULL)
+                    ans.push_back(curr->val);
+                if(curr->left != NULL){
+                    q.push(curr->left);
+                }if(curr->right != NULL){
+                    q.push(curr->right);
+                }
+            }else{
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }
+        }
+        return ans;
+    }
+};
