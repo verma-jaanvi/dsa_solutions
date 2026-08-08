750f13c4754427e4b75fc0fd70d1510c91945cfa
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-08-08 12:04:45 +0530

    Time: 3 ms (67.89%) | Memory: 11.9 MB (81.97%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -24,37 +24,26 @@ public:
     Node* cloneGraph(Node* node) {
         if(!node)   return NULL;
         map<int, Node*> mpp;
-        // vector<bool>    vis(101, false);
         queue<Node*> q;
         Node* root = new Node(node->val);
-        // vis[node->val] = true;
-
-        // vector<Node*> l = node->neighbors;
-        // for(Node* n : l){
-        //     q.push(n);
-        // }
+        
         mpp[node->val] = root;
         q.push(node);
         while(!q.empty()){
             Node* curr = q.front();
             q.pop();
 
-            // Node* nnode = new Node(curr->val);
-            // mpp[curr->val] = nnode;
-            // vis[curr->val] = true;
+            
             vector<Node*> neigh = curr->neighbors;
             
             for(Node* n : neigh){
                 if(mpp.find(n->val) == mpp.end()){
-                    // q.push(n);
-                    if(mpp.find(n->val) == mpp.end()){
+                    // if(mpp.find(n->val) == mpp.end()){
                         mpp[n->val] = new Node(n->val);
                         q.push(n);
-                    }
+                    // }
                 }
-                // else{
-                    // nnode->neighbors.push_back(mpp[n->val]);
-                // }
+                
                 mpp[curr->val]->neighbors.push_back(mpp[n->val]);
             }
         }
