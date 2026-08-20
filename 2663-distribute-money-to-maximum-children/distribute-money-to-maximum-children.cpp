a3e58e07eae4942a278c5a45dff5b7fa96d16301
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-08-20 19:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (62.24%) - LeetSync

--- 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.cpp
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int distMoney(int money, int children) {
-        if (money < children) {
-            return -1;
-        }
+        if(money < children)    return -1;
+
         money -= children;
-        int ans = money / 7;
-        int left = money % 7;
-        if (ans == children && left > 0) {
+        int ans = money/7;
+        money = money%7;
+        if(ans > children){
+            return children -1;
+        }
+        if(money == 3 && (children - ans) == 1){
+            return ans -1;
+        }
+        if(ans == children && money > 0){
             return children - 1;
         }
         
-        if (ans > children) {
-            return children - 1;
-        }
-
-        if (left == 3 && (children - ans) == 1) {
-            return ans - 1;
-        }
-
         return ans;
     }
 };
