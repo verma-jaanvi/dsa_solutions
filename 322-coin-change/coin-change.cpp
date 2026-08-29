dd55a7dcd7d0f84636798fb047de37ae32753640
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-08-29 22:21:46 +0530

    Time: 47 ms (35.70%) | Memory: 49.5 MB (25.19%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        // int ans = 0;
-        // sort(coins.begin(), coins.end());
-        vector<int> dp(amount+1, amount +1);
-        dp[0] = 0;
-        for(int i= 1; i<=amount; ++i){
-            for(int coin : coins){
-                if(i-coin >= 0)
-                    dp[i] = min(dp[i], dp[i-coin]+1);
-            }
-            
+    int fn(int idx, vector<int> &coins, int amt, vector<vector<int>> &dp){
+        if(idx == 0){
+            if(amt % coins[0] == 0) return amt/coins[0];
+            return 1e9;
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(dp[idx][amt] != -1)  return dp[idx][amt];
+        int nt = fn(idx-1, coins, amt, dp);
+        int t = 1e9;
+        if(coins[idx] <= amt)  t = 1+ fn(idx, coins, amt-coins[idx], dp);
+        return dp[idx][amt] = min(nt, t);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> dp(coins.size() , vector<int>(amount+1, -1));
+        int ans = fn(coins.size() - 1, coins, amount, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
