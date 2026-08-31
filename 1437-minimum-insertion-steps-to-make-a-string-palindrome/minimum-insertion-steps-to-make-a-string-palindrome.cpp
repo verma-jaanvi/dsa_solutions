b06c7c4284bbe33a30174121b45b1e5395903df1
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-08-31 18:55:14 +0530

    Time: 39 ms (20.55%) | Memory: 34.7 MB (16.24%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int fn(vector<vector<int>> &dp, string &s, int i, int j){
-        if(i >= j)  return 0;
-        
-        if(dp[i][j] != -1)  return dp[i][j];
-        
-        if(s[i] == s[j])    return dp[i][j] = fn(dp,s, i+1, j-1);
-        return dp[i][j] = min(fn(dp,s, i+1, j), fn(dp, s, i, j-1)) + 1;
-    }
-
     int minInsertions(string s) {
-        // preserve the order
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return fn(dp, s, 0, n-1);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+
+        for(int i =0 ; i<= n;++i){
+            dp[i][0] = 0;
+            dp[0][i] = 0;
+        }
+        string rs = "";
+        for(char ch : s)    rs = ch + rs;
+
+        int ans = 0;
+        for(int i =1; i<=n; ++i){
+            for(int j = 1; j<=n; ++j){
+                if(s[i-1] == rs[j-1]){    
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    ans = max(ans, dp[i][j]);
+                }
+                else{ 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    
+                }
+            }
+        }
+        return n - ans;
     }
 };
