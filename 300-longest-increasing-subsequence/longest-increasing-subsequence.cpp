e2fcb0803df55a6f9229188b2287cd0f441ef24e
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-09-05 14:25:06 +0530

    Time: 37 ms (73.62%) | Memory: 14.3 MB (61.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int fn(int idx, vector<int> &nums, int prev, vector<vector<int>> &dp){
-        if(idx == nums.size())  return 0;
-
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-        int nt = fn(idx+1, nums, prev, dp);
-        int t = 0;
-        if(prev == -1 || nums[idx] > nums[prev])   t = 1+fn(idx+1, nums, idx, dp);
-
-        return dp[idx][prev+1] = max(nt, t);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return fn(0, nums, -1, dp);
+        vector<int> dp(n, 1);
+
+        for(int i = 0; i<n; ++i){
+            int maxi = dp[i];
+            for(int j = 0; j<i; ++j){
+                if(nums[i] > nums[j])
+                    maxi = max(maxi, dp[j]);
+            }
+            dp[i] = dp[i] + maxi;
+            
+        }
+        int ans = 0;
+        for(int i : dp) ans = max(ans, i);
+        return ans - 1;
     }
 };
