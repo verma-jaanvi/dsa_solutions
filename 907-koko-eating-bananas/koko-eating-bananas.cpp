772ed1dd81543cec88f7b3fb91e87e8537daf8df
Jahnvi <164798362+verma-jaanvi@users.noreply.github.com>
2026-09-13 23:20:56 +0530

    Time: 22 ms (5.58%) | Memory: 23.1 MB (5.71%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int st = 1, en = 0;
-        int n = piles.size();
+        int l = 1, r = piles[0];
         for(int i : piles){
-            en = max(en, i);
+            r = max(r, i);
         }
         int ans = 0;
-        while(st <= en){
-            int mid = st + (en - st)/2;
+        while( l  <= r){
+            int mid = l + (r-l)/2;
             int cnt = 0;
             for(int i : piles){
                 cnt += ceil((double)i/mid);
-                if(cnt > h)    break;
+
+                if(cnt > h) break;
             }
+
             if(cnt <= h){
                 ans = mid;
-                en = mid - 1;
+                r = mid -1;
             }else{
-                st = mid + 1;
+                l = mid + 1;
             }
         }
         return ans;
